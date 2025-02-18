083e127af8fe00892d713456dadef5988c5a9bb4
OwlHaus <kevin@owlhouse.io>
2025-02-18 08:46:13 -0800

    wasn't working because I used the wrong pins...

--- bin/firmware/config.h
+++ bin/firmware/config.h
@@ -14,9 +14,9 @@
 #define EAST_FINGER1 (2)
 #define EAST_FINGER2 (3)
 
-#define EAST_STEP   (7)
-#define EAST_DIR    (8)
-#define EAST_ENABLE (6)
+#define EAST_STEP   (8)
+#define EAST_DIR    (9)
+#define EAST_ENABLE (7)
 #define EAST_HOMING (15)
 
 // South Claw
